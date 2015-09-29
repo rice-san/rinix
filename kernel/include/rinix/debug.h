27ee80ffec2dad1dc43c20ea10b708cf7728cb46
Rodie Martha <rodie98.2@gmail.com>
2015-09-29 12:14:07 -0400

    Yep

---
+++ kernel/include/rinix/debug.h
@@ -0,0 +1,27 @@
+#ifndef _RINIX_DEBUG_H
+#define _RINIX_DEBUG_H
+
+#include <stdio.h>
+
+// Uncomment this for debug
+#define DEBUG 1
+
+#define nullAction( ) \
+do { ; } \
+while(0)
+
+#ifdef DEBUG
+
+	#define printd(str, arg...) ( printf( (str) , ##arg ) )
+	#define _debug(action) \
+	do { action; } \
+	while(0)
+
+#else
+	
+	#define printd(str, arg...) nullAction( )
+	#define _debug(action) nullAction( )
+	
+#endif
+
+#endif
--- kernel/rinix/rinix.c
+++ kernel/rinix/rinix.c
@@ -9,6 +9,7 @@
 #include <mm/mem.h>
 
 #include <rinix/arch_init.h>
+#include <rinix/debug.h>
 #include <rinix/multiboot.h>
 #include <rinix/pause.h>
 #include <rinix/version.h>
@@ -31,42 +32,43 @@ void initk(multiboot_info_t *mbd, uint32_t initialPD)
 	puts("(c) Copyright 2015 Royston Martha. All Rights Reserved.");
 	printf("rinix-%s v%d.%d.%d\n", ARCH_NAME, _RINIX_VERSION_MAJOR, _RINIX_VERSION_MINOR, _RINIX_VERSION_RELEASE);
 	// Check multiboot structure for errors 
-	printf("Multiboot Info: %x\n", &mbd);
-	term_setcolor(make_color(COLOR_CYAN, COLOR_BLACK));
-	printf("Memory Map At: %x\n", mbd->mmap_addr);
-	printf("Memory Map Length: %x\n", mbd->mmap_length);
+	printd("Multiboot Info: %x\n", &mbd);
+	_debug( term_setcolor(make_color(COLOR_CYAN, COLOR_BLACK)) );
+	printd("Memory Map At: %x\n", mbd->mmap_addr);
+	printd("Memory Map Length: %x\n", mbd->mmap_length);
 	if((mbd->flags & 0x40))
 	{
-		printf("Memory Length: %x\n", mbd->mem_upper);
+		printd("Memory Length: %x\n", mbd->mem_upper);
 		multiboot_memory_map_t* mmap = mbd->mmap_addr;
-		printf("Memory Map Info: \n==================\n");
+		printd("Memory Map Info: \n==================\n");
 		while(mmap < mbd->mmap_addr + mbd->mmap_length) {
 			
-			printf("Memory Map: start=%x length=%x size=%x value=%x\n", (unsigned long)mmap->addr, (unsigned long)mmap->len, (unsigned long)mmap->size, mmap->type);
+			printd("Memory Map: start=%x length=%x size=%x value=%x\n", (unsigned long)mmap->addr, (unsigned long)mmap->len, (unsigned long)mmap->size, mmap->type);
 			mmap = (multiboot_memory_map_t*) ( (unsigned int)mmap + mmap->size + sizeof(unsigned int) );
 		}
 	
 	}
 	else if((mbd->flags & 1))
 	{
-		printf("Memory Length: %x\n", mbd->mem_upper);
+		printd("Memory Length: %x\n", mbd->mem_upper);
 	}
 	else
 	{
 		puts("Fatal Error: No valid memory info. Halted.");
 		for(;;);
 	}
+	_debug( term_setcolor(make_color(COLOR_LIGHT_GREY, COLOR_BLACK)) );
 }
 
 void kmain(void)
 {
 	
 	// printf("Architecture: %s\n", ARCH_NAME);
-	printf("Page Directory is at address: %x\n", initialPageDirectory);
-	printf("Kernel Begins at %x\n", &kernel_start);
-	printf("Kernel Ends at: %x\n", &kernel_end);
+	printd("Page Directory is at address: %x\n", initialPageDirectory);
+	printd("Kernel Begins at %x\n", &kernel_start);
+	printd("Kernel Ends at: %x\n", &kernel_end);
 	set_pgt_entry(0, 0);
-	printf("%x\n", get_pgt_entry(0));
-	printf("%x\n", get_pgt_entry(0xC0000000));
+	printd("%x\n", get_pgt_entry(0));
+	printd("%x\n", get_pgt_entry(0xC0000000));
 	for(;;);
 }
